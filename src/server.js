67f040e93dbab7c9065839903b7d7664d1f459f8
CruellaDeVil <dev.timetravel@gmail.com>
2021-12-27 18:41:03 +0900

    made socket nickname

--- src/server.js
+++ src/server.js
@@ -20,12 +20,21 @@ const sockets = [];
 
 wss.on("connection", (socket) => {
   sockets.push(socket);
+  socket["nickname"] = "anonymous";
   console.log("✅ Connected to Browser");
   socket.on("close", () => {
     console.log("❌ Disconnected from Browser");
   });
-  socket.on("message", (message) => {
-    sockets.forEach((aSocket) => aSocket.send(message.toString()));
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload.toString()}`));
+        break;
+      case "nickname":
+        socket["nickname"] = message.payload;
+        break;
+    }
   });
 });
 
